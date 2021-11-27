93a84335475c050b465b423ee7e96039927da15c
mhw0 <mhw0@yahoo.com>
2021-11-27 20:50:51 +0500

    keccak256: type alias for `HashReturn`

--- include/ethc/keccak256.h
+++ include/ethc/keccak256.h
@@ -2,11 +2,8 @@
 #define ETHC_KECCAK256_H
 #include <KeccakHash.h>
 
-#define KECCAK_RATE 1088
-#define KECCAK_CAPACITY 512
-#define KECCAK_HASHBITLEN 256
-#define KECCAK_DELIMITED_SUFFIX 0x1
+typedef HashReturn KeccakHashReturn;
 
-HashReturn eth_keccak256(const char* input, unsigned char* output);
+KeccakHashReturn eth_keccak256(const char *input, unsigned char *output);
 
 #endif
--- src/keccak256.c
+++ src/keccak256.c
@@ -1,14 +1,14 @@
 #include <assert.h>
 #include <ethc/keccak256.h>
 
-HashReturn eth_keccak256(const char* input, unsigned char* output) {
+KeccakHashReturn eth_keccak256(const char *input, unsigned char *output) {
   assert(input != NULL);
   Keccak_HashInstance instance;
 
-  if(Keccak_HashInitialize(&instance, KECCAK_RATE, KECCAK_CAPACITY, KECCAK_HASHBITLEN, KECCAK_DELIMITED_SUFFIX) == KECCAK_FAIL)
+  if (Keccak_HashInitialize(&instance, 1088, 512, 256, 0x1) == KECCAK_FAIL)
     return KECCAK_FAIL;
 
-  if(Keccak_HashUpdate(&instance, input, strlen(input) * 8) == KECCAK_FAIL)
+  if (Keccak_HashUpdate(&instance, input, strlen(input) * 8) == KECCAK_FAIL)
     return KECCAK_FAIL;
 
   return Keccak_HashFinal(&instance, output);
