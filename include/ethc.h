e96db96d843b0387a44020d36cc83e720ea447d8
mhw0 <mhw0@yahoo.com>
2023-08-25 19:44:47 +0500

    rlp: import `rlp.h` in `ethc.h`

--- include/ethc.h
+++ include/ethc.h
@@ -1,11 +1,12 @@
 #ifndef ETHC_H
 #define ETHC_H
-#include <ethc/address.h>
 #include <ethc/abi.h>
 #include <ethc/account.h>
+#include <ethc/address.h>
 #include <ethc/bloom.h>
 #include <ethc/ecdsa.h>
-#include <ethc/unit.h>
 #include <ethc/hex.h>
 #include <ethc/keccak256.h>
+#include <ethc/rlp.h>
+#include <ethc/unit.h>
 #endif
